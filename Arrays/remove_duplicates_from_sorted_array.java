f02a77b4211a7ba013a27764fbe2c8f1548d2ce6
BhumiPanchal95 <117275104+HappyPanchal22@users.noreply.github.com>
2025-11-27 22:09:48 -0500

    Solved remove_duplicates_from_sorted_array.java

---
+++ Arrays/remove_duplicates_from_sorted_array.java
@@ -0,0 +1,25 @@
+/**
+ * 80. Remove Duplicates from Sorted Array II
+ * Difficulty: Medium
+ 
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * 
+ * Tags: Array
+ * Date Solved: 2024-11-27
+ */
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 2;                 // First 2 elements always valid
+        for(int i = 2; i < nums.length; i++){
+             // Place element if: haven't placed 2 yet OR different from element 2 back
+            if(k < 2 || nums[i] != nums[k-2]){
+            nums[k] = nums[i];
+            k++;
+            }
+        }
+        return k;
+    }  
+}
